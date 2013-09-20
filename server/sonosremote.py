e5f6bfe5bd06eee943a90bc7a868962e8e670806
Cosmin Manoliu <cosmin@Cosmins-iMac.local>
2013-09-20 21:57:33 +0300

    able to send play command

--- server/sonosremote.py
+++ server/sonosremote.py
@@ -2,25 +2,35 @@ import os
 import sys
 import webapp2
 from soco import SoCo
-
+import json
+import logging, traceback
+logger = logging.getLogger(__name__)
 
 class MainPage(webapp2.RequestHandler):
 
+      
+    sonos = SoCo('bfree.ro') # Pass in the IP of your Sonos speaker
+    
     def post(self):
-        #self.response.write(self.request.body)
-        sonos = SoCo('bfree.ro') # Pass in the IP of your Sonos speaker
+    
+        CMD_PLAY = 1  
+        command = json.loads((self.request.body))
+        if command['1'] == CMD_PLAY:
+            return self.play()
+        else:
+            return self.error()
 
         # Pass in a URI to a media file to have it streamed through the Sonos speaker
         #sonos.play_uri('http://archive.org/download/TenD2005-07-16.flac16/TenD2005-07-16t10Wonderboy_64kb.mp3')
 
-        track = sonos.get_current_track_info()
+        #track = sonos.get_current_track_info()
 
-        self.response.write(track)
+        #self.response.write(track)
         
-        queue = sonos.get_queue()
-        for item in queue:
-            self.response.write(item['title'])
-            self.response.write('\r\n')
+        #queue = sonos.get_queue()
+        #for item in queue:
+        #    self.response.write(item['title'])
+        #    self.response.write('\r\n')
         
         #sonos.pause()
 
@@ -31,7 +41,16 @@ class MainPage(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Sonos Remote Control')
 
-
+    def play(self):
+        logger.info('Play')
+        self.sonos.play()
+    
+    def stop(self):
+        self.sonos.stop()
+        
+    def error(self):
+        self.sonos.stop()
+        
 def fix_path():
     # credit:  Nick Johnson of Google
     sys.path.append(os.path.join(os.path.dirname(__file__), 'requests'))
--- server/test.sh
+++ server/test.sh
@@ -1,2 +1,2 @@
-#curl -i -X POST -d '{"1":444325,"2":261038,"3":"auto"}' http://localhost:8080
-curl -i -X POST -d '{"1":444325,"2":261038,"3":"auto"}' http://sonospebbleremote.appspot.com/
+curl -i -X POST -d '{"1":1}' http://localhost:8080
+#curl -i -X POST -d '{"1":444325,"2":261038,"3":"auto"}' http://sonospebbleremote.appspot.com/
