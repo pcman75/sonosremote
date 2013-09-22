4781b9326e10e982841f28ebb1d9fc234e9dba7b
Cosmin Manoliu <cosmin@Cosmins-iMac.local>
2013-09-23 00:16:43 +0300

    test

--- server/test/play_google.sh
+++ server/test/play_google.sh
@@ -1,1 +1,1 @@
-curl -i -X POST -d '{"1":1}' http://sonospebbleremote.appspot.com
+curl --user-agent "httpebble/1.0 CFNetwork/672.0.2 Darwin/14.0.0" -i -X POST -d '{"1":1}' http://sonospebbleremote.appspot.com
