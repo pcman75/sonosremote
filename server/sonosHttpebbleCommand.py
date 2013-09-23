e0bed6e0c5898b9a8bf14440931abd0bdaa51861
pcman <pcman75@me.com>
2013-09-23 17:27:30 +0300

    work in progress

---
+++ server/sonosHttpebbleCommand.py
@@ -0,0 +1,85 @@
+from soco import SoCo
+import json
+import logging, traceback
+logger = logging.getLogger(__name__)
+
+class SonosHttpebbleCommand():
+
+    CMD_PLAY = 1
+    CMD_STOP = 2
+    CMD_PAUSE = 3
+    CMD_PREV = 4
+    CMD_NEXT = 5
+    CMD_INFO = 6
+    
+    TRACK_INFO_ARTIST = 3
+    TRACK_INFO_ALBUM = 4
+    TRACK_INFO_TITLE = 5
+    TRACK_INFO_POSITION = 6
+    TRACK_INFO_ALBUM_ART = 7
+    
+    sonos = SoCo('bfree.ro') # Pass in the IP of your Sonos speaker
+    
+    def callCommand(self, commandString):
+        
+        command = json.loads(commandString)
+        
+        if command['1'] == self.CMD_PLAY:
+            commandResult = self.play()
+        elif command['1'] == self.CMD_STOP:
+            commandResult = self.stop()
+        elif command['1'] == self.CMD_PAUSE:
+            commandResult = self.pause() 
+        elif command['1'] == self.CMD_PREV:
+            commandResult = self.previous()                    
+        elif command['1'] == self.CMD_NEXT:
+            commandResult = self.next() 
+        elif command['1'] == self.CMD_INFO:
+            commandResult = self.getCurrentTrackInfo() 
+        else:
+            commandResult = False;
+        
+        return self.make_json_response(commandResult, command['1'])
+    
+    def make_json_response(self, commandResult, command):
+        if(commandResult == True):
+            commandResult = 1
+        else:
+            commandResult = 0           
+        return json.dumps({"1": command, "2": ["b", commandResult]})
+    
+    def play(self):
+        print "Play"
+        return self.sonos.play()
+    
+    def stop(self):
+        print "Stop"
+        return self.sonos.stop()
+
+    def pause(self):
+        return self.sonos.pause()
+        
+    def previous(self):
+        return self.sonos.previous()
+        
+    def next(self):
+        return self.sonos.next()
+        
+    def error(self):
+        return self.sonos.stop()
+    
+    def getCurrentTrackInfo(self):
+        track = self.sonos.get_current_track_info()
+        trackInfo = {}
+        trackInfo[str(self.TRACK_INFO_TITLE)] = track['title']
+        trackInfo[str(self.TRACK_INFO_ARTIST)] = track['artist']
+        trackInfo[str(self.TRACK_INFO_ALBUM)] = track['album']
+        trackInfo[str(self.TRACK_INFO_POSITION)] = track['position']
+        trackInfo[str(self.TRACK_INFO_ALBUM_ART)] = track['album_art']
+        print trackInfo
+        
+if __name__ == "__main__":
+    test = SonosHttpebbleCommand()
+    #print test.callCommand('{"1":1}')
+    #print test.callCommand('{"1":2}')
+    print test.callCommand('{"1":6}')
--- server/sonosremote.py
+++ server/sonosremote.py
@@ -1,42 +1,32 @@
 import os
 import sys
 import webapp2
-from soco import SoCo
-import json
 import logging, traceback
 logger = logging.getLogger(__name__)
 
-CMD_PLAY = 1
-CMD_STOP = 2
-CMD_PAUSE = 3
-CMD_PREV = 4
-CMD_NEXT = 5
-
 class MainPage(webapp2.RequestHandler):
 
-      
-    sonos = SoCo('bfree.ro') # Pass in the IP of your Sonos speaker
-    
     def post(self):
     
-          
-        command = json.loads((self.request.body))
+        command = self.request.body
+        httPebbleCommand = SonosHttPebbleCommand()
+        response = httPebbleCommand.callCommand(command)
         
-        if command['1'] == CMD_PLAY:
-            commandResult = self.play()
-        elif command['1'] == CMD_STOP:
-            commandResult = self.stop()
-        elif command['1'] == CMD_PAUSE:
-            commandResult = self.pause() 
-        elif command['1'] == CMD_PREV:
-            commandResult = self.previous()                    
-        elif command['1'] == CMD_NEXT:
-            commandResult = self.next() 
-        else:
-            commandResult = False;
+        self.response.headers['Content-Type'] = 'application/json'
+        return self.response.write(response)       
+   
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Sonos Remote Control')
+        
+def fix_path():
+    # credit:  Nick Johnson of Google
+    sys.path.append(os.path.join(os.path.dirname(__file__), 'requests'))
+    
+fix_path()
+application = webapp2.WSGIApplication([('/', MainPage),], debug=True)
 
-        return self.make_response(commandResult, command['1'])
-        
+
         # Pass in a URI to a media file to have it streamed through the Sonos speaker
         #sonos.play_uri('http://archive.org/download/TenD2005-07-16.flac16/TenD2005-07-16t10Wonderboy_64kb.mp3')
 
@@ -54,41 +44,3 @@ class MainPage(webapp2.RequestHandler):
         # Play a stopped or paused track
         #sonos.play()
 
-    def make_response(self, commandResult, command):
-        logger.info(commandResult)
-        if(commandResult == True):
-            commandResult = 1
-        else:
-            commandResult = 0           
-        self.response.write(json.dumps({"1": command, "2": ["b", commandResult]}))
-    
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Sonos Remote Control')
-
-
-
-    def play(self):
-        return self.sonos.play()
-    
-    def stop(self):
-        return self.sonos.stop()
-
-    def pause(self):
-        return self.sonos.pause()
-        
-    def previous(self):
-        return self.sonos.previous()
-        
-    def next(self):
-        return self.sonos.next()
-        
-    def error(self):
-        return self.sonos.stop()
-        
-def fix_path():
-    # credit:  Nick Johnson of Google
-    sys.path.append(os.path.join(os.path.dirname(__file__), 'requests'))
-    
-fix_path()
-application = webapp2.WSGIApplication([('/', MainPage),], debug=True)
