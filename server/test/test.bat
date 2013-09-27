1d5af54acc3b696a97d4385090c6c36951c40f55
pcman <pcman75@me.com>
2013-09-27 16:55:49 +0300

    test

--- server/test/test.bat
+++ server/test/test.bat
@@ -1,2 +1,1 @@
-curl -i -X POST -d '{"1":444325,"2":261038,"3":"auto"}' http://localhost:8080
-#curl -i -X POST -d '{"1":444325,"2":261038,"3":"auto"}' http://sonospebbleremote.appspot.com/
+curl -i -X POST -d {\"1\":444325,\"2\":261038,\"3\":\"auto\"} http://bfree.ro/thermometer/httpebble.php
